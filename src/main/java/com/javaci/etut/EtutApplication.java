7ff36de003301137637c5a0ebda84ea6bb21b5d4
serhat nebiler <serhatnebiler@gmail.com>
2021-03-31 23:23:30 +0300

    init

---
+++ src/main/java/com/javaci/etut/EtutApplication.java
@@ -0,0 +1,13 @@
+package com.javaci.etut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaci/etut/EtutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaci.etut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
